2ce4f96e97ed0a14edfc2f84310a1229d915c72e
snapppy <forever.spyder@gmail.com>
2017-01-12 09:22:21 -0700

    01/12/17 Initial commit of project files

---
+++ AllEventsTicketing/Set.js
@@ -0,0 +1,56 @@
+function Set() {
+	
+	
+	this.intersection = function(listA, listB) {
+    
+	   var resultList = new Array();
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+	   
+	   /*-------------------------------Insert your code here -------------------------------------*/
+       
+	   return resultList;
+	}
+    
+    
+    
+	this.union = function(listA, listB) {
+
+	   var resultList = new Array();
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+	   
+	   /*-------------------------------Insert your code here -------------------------------------*/ 
+	   
+	   return resultList;
+	}
+
+
+
+
+	this.relativeCompliment = function(listA, listB) {
+
+	   var resultList = new Array();
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+	   	   
+	   /*-------------------------------Insert your code here -------------------------------------*/
+       
+	   return resultList;
+	}
+
+
+
+	this.symetricDifference = function(listA, listB) {
+
+	   var resultList = new Array();
+       
+	   /*-------------------------------Insert your code here -------------------------------------*/
+
+	   /*-------------------------------Insert your code here -------------------------------------*/
+       
+	   return resultList;
+	}	
+	
+
+}
