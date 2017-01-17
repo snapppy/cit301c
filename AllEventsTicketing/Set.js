33cc2ea8717cad992ff4e71c64ecf48f03aae5f9
snapppy <forever.spyder@gmail.com>
2017-01-17 08:09:58 -0700

    01/17/17 All joins working as expected.

--- AllEventsTicketing/Set.js
+++ AllEventsTicketing/Set.js
@@ -2,13 +2,25 @@ function Set() {
 	
 	
 	this.intersection = function(listA, listB) {
-    
-	   var resultList = new Array();
-       
-	   /*-------------------------------Insert your code here -------------------------------------*/
-	   
-	   /*-------------------------------Insert your code here -------------------------------------*/
-       
+
+       if (listA === null || listB === null) {
+           return null;
+       }
+
+       var resultList = [];
+
+       var arrayLength = listA.length;
+       for (var i = 0; i < arrayLength; i++) {
+           var nextValue = listA[i];
+
+           for (var j = 0; j < listB.length; j++) {
+               if (listB[j] === nextValue) {
+                   resultList.push(listB[j]);
+                   break;
+               }
+           }
+       }
+
 	   return resultList;
 	}
     
@@ -16,12 +28,16 @@ function Set() {
     
 	this.union = function(listA, listB) {
 
-	   var resultList = new Array();
-       
-	   /*-------------------------------Insert your code here -------------------------------------*/
-	   
-	   /*-------------------------------Insert your code here -------------------------------------*/ 
-	   
+        if (listA === null || listB === null) {
+            return null;
+        }
+
+        var resultList = [];
+        this.append(resultList,listA);
+
+        var relativeComp = this.relativeCompliment(listB, listA);
+        this.append(resultList,relativeComp);
+
 	   return resultList;
 	}
 
@@ -30,11 +46,26 @@ function Set() {
 
 	this.relativeCompliment = function(listA, listB) {
 
-	   var resultList = new Array();
-       
-	   /*-------------------------------Insert your code here -------------------------------------*/
-	   	   
-	   /*-------------------------------Insert your code here -------------------------------------*/
+        if (listA === null || listB === null) {
+            return null;
+        }
+
+        if (listB.length === 0) {
+            return listA;
+        }
+
+        var resultList = [];
+
+        for (var i = 0; i < listA.length; i++) {
+            for (var j = 0; j < listB.length; j++) {
+                if (listA[i] === listB[j]) {
+                    break;
+                }
+                if (listB[j + 1] == undefined) {
+                    resultList.push(listA[i]);
+                }
+            }
+        }
        
 	   return resultList;
 	}
@@ -43,14 +74,23 @@ function Set() {
 
 	this.symetricDifference = function(listA, listB) {
 
-	   var resultList = new Array();
-       
-	   /*-------------------------------Insert your code here -------------------------------------*/
+        if (listA === null || listB === null) {
+            return null;
+        }
 
-	   /*-------------------------------Insert your code here -------------------------------------*/
-       
+	    var resultList = [];
+	    var relativeComplimentResult = this.relativeCompliment(listA, listB);
+        this.append(resultList,relativeComplimentResult);
+
+	    relativeComplimentResult = this.relativeCompliment(listB, listA);
+        this.append(resultList,relativeComplimentResult);
+
 	   return resultList;
-	}	
-	
+	}
 
+	this.append = function(resultList, list) {
+        for (var i = 0; i < list.length; i++) {
+            resultList.push(list[i]);
+        }
+    }
 }
