42b22c3d862ea4f188476649660cb831970a57b7
snapppy <forever.spyder@gmail.com>
2017-01-24 19:46:04 -0700

    01/24/17 - WeLearn CMS finished for week 2 assignment.

---
+++ recipe-book/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+
+@Component({
+  moduleId: module.id,
+  selector: 'rb-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+}
